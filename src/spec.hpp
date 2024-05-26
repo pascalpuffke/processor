bd3dccc881724d4fa1955b3f51902d59b894ea51
Pascal Puffke <pascal@pascalpuffke.de>
2024-05-26 16:18:35 +0200

    Add spec file missing from last commit

---
+++ src/spec.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <types.hpp>
+
+namespace ProcessorSpec {
+    using reg_t = u8;
+    using imm_t = u8;
+    using data_t = u8;
+    using insr_t = u16;
+    using addr_t = u16;
+
+    static constexpr auto register_count = 8;
+    // Note that the stack grows downwards
+    static constexpr auto stack_size = 0xFF;
+    static constexpr ProcessorSpec::addr_t stack_top_addr = 0x00FF;
+    static constexpr ProcessorSpec::addr_t highest_addr = std::numeric_limits<addr_t>::max();
+    static constexpr ProcessorSpec::addr_t reset_pc = 0xFF00;
+};
